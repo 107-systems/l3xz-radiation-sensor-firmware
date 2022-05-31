b91b6877f3c230e3ba5207d46e89fc477605f58f
Alexander Entinger <cto@lxrobotics.com>
2022-05-31 09:08:44 +0200

    Works.

--- l3xz-fw_radiation_sensor.ino
+++ l3xz-fw_radiation_sensor.ino
@@ -21,7 +21,6 @@
 
 #include <ArduinoUAVCAN.h>
 #include <ArduinoMCP2515.h>
-#include <I2C_eeprom.h>
 #include <Adafruit_SleepyDog.h>
 
 /**************************************************************************************
@@ -32,7 +31,6 @@
 #define LED2_PIN A7
 #define LED3_PIN A6
 #define RADIATION_PIN 10
-#define ANALOG_PIN A1
 
 /**************************************************************************************
  * NAMESPACE
@@ -48,9 +46,7 @@ using namespace uavcan::primitive::scalar;
 static int          const MKRCAN_MCP2515_CS_PIN  = 3;
 static int          const MKRCAN_MCP2515_INT_PIN = 9;
 
-static CanardPortID const ID_INPUT_VOLTAGE       = 1001U;
-static CanardPortID const ID_LED1                = 1005U;
-static CanardPortID const ID_RADIATION_VALUE     = 3000U;
+static CanardPortID const ID_RADIATION_VALUE = 3000U;
 
 static SPISettings  const MCP2515x_SPI_SETTING{1000000, MSBFIRST, SPI_MODE0};
 
@@ -58,13 +54,12 @@ static SPISettings  const MCP2515x_SPI_SETTING{1000000, MSBFIRST, SPI_MODE0};
  * FUNCTION DECLARATION
  **************************************************************************************/
 
-void onLed1_Received (CanardTransfer const &, ArduinoUAVCAN &);
-
 /**************************************************************************************
  * GLOBAL VARIABLES
  **************************************************************************************/
 
 static ArduinoUAVCAN * uc = nullptr;
+
 
 ArduinoMCP2515 mcp2515([]()
                        {
@@ -83,10 +78,8 @@ ArduinoMCP2515 mcp2515([]()
                        [](CanardFrame const & f) { uc->onCanFrameReceived(f); },
                        nullptr);
 
-I2C_eeprom ee(0x50, I2C_DEVICESIZE_24LC64);
-
 Heartbeat_1_0<> hb;
-volatile int radiation_ticks=0;
+volatile int radiation_ticks = 0;
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -97,7 +90,7 @@ void setup()
   Watchdog.enable(1000);
 
   Serial.begin(115200);
-  //while(!Serial) { } /* only for debug */
+  while(!Serial) { Watchdog.reset(); } /* only for debug */
 
   /* Setup LED pins and initialize */
   pinMode(LED1_PIN, OUTPUT);
@@ -108,19 +101,7 @@ void setup()
   digitalWrite(LED3_PIN, LOW);
   pinMode(RADIATION_PIN, INPUT_PULLUP);
 
-  /* Setup I2C Eeprom */
-  ee.begin();
-  if (! ee.isConnected())
-  {
-    Serial.println("ERROR: Can't find eeprom\nstopped...");
-    while (1);
-  }
-  uint8_t const eeNodeID=ee.readByte(0);
-  Serial.print("Node-ID from eeprom: ");
-  Serial.println(eeNodeID);
-
-  /* create UAVCAN class */
-  uc = new ArduinoUAVCAN(eeNodeID, [](CanardFrame const & frame) -> bool { return mcp2515.transmit(frame); });
+  uc = new ArduinoUAVCAN(98, [](CanardFrame const & frame) -> bool { return mcp2515.transmit(frame); });
 
   /* Setup SPI access */
   SPI.begin();
@@ -143,12 +124,11 @@ void setup()
   hb.data.vendor_specific_status_code = 0;
 
   /* Subscribe to the reception of Bit message. */
-  uc->subscribe<Bit_1_0<ID_LED1>>(onLed1_Received);
   Serial.println("init finished");
 
   /* set up radiation measurement */
   attachInterrupt(digitalPinToInterrupt(RADIATION_PIN), radiation_count, RISING);
-  radiation_ticks=0;
+  radiation_ticks = 0;
 
   /* Feed the watchdog to keep it from biting. */
   Watchdog.reset();
@@ -182,7 +162,6 @@ void loop()
    */
   static unsigned long prev_heartbeat = 0;
   static unsigned long prev_radiation = 0;
-  static unsigned long prev_battery_voltage = 0;
 
   unsigned long const now = millis();
 
@@ -199,23 +178,14 @@ void loop()
   if((now - prev_radiation) > 10000)
   {
     Integer16_1_0<ID_RADIATION_VALUE> uavcan_radiation_value;
+    noInterrupts();
     uavcan_radiation_value.data.value = radiation_ticks;
+    interrupts();
     uc->publish(uavcan_radiation_value);
     prev_radiation = now;
     Serial.print("Radiation Value: ");
     Serial.println(radiation_ticks);
     radiation_ticks=0;
-  }
-
-  if((now - prev_battery_voltage) > (10*1000))
-  {
-    float const analog = analogRead(ANALOG_PIN)*3.3*11.0/1023.0;
-    Serial.print("Analog Pin: ");
-    Serial.println(analog);
-    Real32_1_0<ID_INPUT_VOLTAGE> uavcan_input_voltage;
-    uavcan_input_voltage.data.value = analog;
-    uc->publish(uavcan_input_voltage);
-    prev_battery_voltage = now;
   }
 
   /* Transmit all enqeued CAN frames */
@@ -229,23 +199,9 @@ void loop()
  * FUNCTION DEFINITION
  **************************************************************************************/
 
-void onLed1_Received(CanardTransfer const & transfer, ArduinoUAVCAN & /* uavcan */)
+void radiation_count()
 {
-  Bit_1_0<ID_LED1> const uavcan_led1 = Bit_1_0<ID_LED1>::deserialize(transfer);
-
-  if(uavcan_led1.data.value)
-  {
-    digitalWrite(LED1_PIN, HIGH);
-    Serial.println("Received Bit1: true");
-  }
-  else
-  {
-    digitalWrite(LED1_PIN, LOW);
-    Serial.println("Received Bit1: false");
-  }
-}
-
-void radiation_count() {
   /* simply increase tick counter */
   radiation_ticks++;
+  Serial.println(radiation_ticks);
 }
