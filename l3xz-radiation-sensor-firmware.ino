080fff53148032ba65222c01a563dd0f8281978d
Alexander Entinger <consulting@lxrobotics.com>
2023-04-24 15:49:43 +0200

    Cleanup and adjust to current overall firmware standard.

--- l3xz-radiation-sensor-firmware.ino
+++ l3xz-radiation-sensor-firmware.ino
@@ -1,101 +1,147 @@
-/*
- * Software for the leg controller for the L3X-Z Hexapod
- *
- * Hardware:
- *   - Arduino Nano 33 IoT
- *   - MCP2515
- * https://github.com/107-systems/l3xz-hw_leg-controller
- *
- * Used Subject-IDs
- * 3000 - pub - Integer16 - radiation value
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2023 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz-radiation-sensor-firmware/graphs/contributors.
  */
 
 /**************************************************************************************
  * INCLUDE
  **************************************************************************************/
 
-#include <pico/stdlib.h>
-#include <hardware/watchdog.h>
-
 #include <SPI.h>
+#include <Wire.h>
 
 #include <107-Arduino-Cyphal.h>
+#include <107-Arduino-Cyphal-Support.h>
+
 #include <107-Arduino-MCP2515.h>
-#include <107-Arduino-UniqueId.h>
-#include <107-Arduino-CriticalSection.h>
+#include <107-Arduino-littlefs.h>
+#include <107-Arduino-24LCxx.hpp>
+
+#define DBG_ENABLE_ERROR
+#define DBG_ENABLE_WARNING
+#define DBG_ENABLE_INFO
+#define DBG_ENABLE_DEBUG
+//#define DBG_ENABLE_VERBOSE
+#include <107-Arduino-Debug.hpp>
 
 /**************************************************************************************
  * NAMESPACE
  **************************************************************************************/
 
 using namespace uavcan::node;
-using namespace uavcan::_register;
-using namespace uavcan::primitive::scalar;
 
 /**************************************************************************************
  * CONSTANTS
  **************************************************************************************/
 
-static int const MKRCAN_MCP2515_CS_PIN  = 3;
-static int const MKRCAN_MCP2515_INT_PIN = 9;
-static int const RADIATION_PIN          = 10;
+static uint8_t const EEPROM_I2C_DEV_ADDR = 0x50;
 
-static CanardPortID const ID_RADIATION_VALUE = 3000U;
-static CanardNodeID const DEFAULT_RADIATION_SENSOR_NODE_ID = 21;
+static int const MCP2515_CS_PIN  = 17;
+static int const MCP2515_INT_PIN = 20;
+static int const RADIATION_PIN   = 10;
+static int const LED2_PIN        = 21; /* GP21 */
+static int const LED3_PIN        = 22; /* GP22 */
 
-static SPISettings  const MCP2515x_SPI_SETTING{1000000, MSBFIRST, SPI_MODE0};
+static SPISettings const MCP2515x_SPI_SETTING{10*1000*1000UL, MSBFIRST, SPI_MODE0};
+
+static uint16_t const UPDATE_PERIOD_HEARTBEAT_ms = 1000;
+static uint16_t const UPDATE_PERIOD_RADIATION_ms = 1000;
 
 /**************************************************************************************
  * FUNCTION DECLARATION
  **************************************************************************************/
 
-void radiation_count();
-void onReceiveBufferFull(CanardFrame const &);
+void onReceiveBufferFull(CanardFrame const & frame);
+ExecuteCommand::Response_1_1 onExecuteCommand_1_1_Request_Received(ExecuteCommand::Request_1_1 const &);
 
 /**************************************************************************************
  * GLOBAL VARIABLES
  **************************************************************************************/
 
-ArduinoMCP2515 mcp2515([]()
-                       {
-                         SPI.beginTransaction(MCP2515x_SPI_SETTING);
-                         digitalWrite(MKRCAN_MCP2515_CS_PIN, LOW);
-                       },
-                       []()
-                       {
-                         digitalWrite(MKRCAN_MCP2515_CS_PIN, HIGH);
-                         SPI.endTransaction();
-                       },
+DEBUG_INSTANCE(80, Serial);
+
+ArduinoMCP2515 mcp2515([]() { digitalWrite(MCP2515_CS_PIN, LOW); },
+                       []() { digitalWrite(MCP2515_CS_PIN, HIGH); },
                        [](uint8_t const d) { return SPI.transfer(d); },
                        micros,
                        onReceiveBufferFull,
                        nullptr);
 
 Node::Heap<Node::DEFAULT_O1HEAP_SIZE> node_heap;
-Node node_hdl(node_heap.data(), node_heap.size(), micros, [] (CanardFrame const & frame) { return mcp2515.transmit(frame); }, DEFAULT_RADIATION_SENSOR_NODE_ID);
+Node node_hdl(node_heap.data(), node_heap.size(), micros, [] (CanardFrame const & frame) { return mcp2515.transmit(frame); });
 
-Publisher<Heartbeat_1_0> heartbeat_pub = node_hdl.create_publisher<Heartbeat_1_0>
-  (Heartbeat_1_0::_traits_::FixedPortId, 1*1000*1000UL /* = 1 sec in usecs. */);
+Publisher<Heartbeat_1_0> heartbeat_pub = node_hdl.create_publisher<Heartbeat_1_0>(1*1000*1000UL /* = 1 sec in usecs. */);
 
-Publisher<Integer16_1_0> radiation_tick_pub = node_hdl.create_publisher<Integer16_1_0>
-  (ID_RADIATION_VALUE, 1*1000*1000UL /* = 1 sec in usecs. */);
+Publisher<uavcan::primitive::scalar::Natural16_1_0> radiation_pub;
+
+ServiceServer execute_command_srv = node_hdl.create_service_server<ExecuteCommand::Request_1_1, ExecuteCommand::Response_1_1>(2*1000*1000UL, onExecuteCommand_1_1_Request_Received);
+
+/* LITTLEFS/EEPROM ********************************************************************/
+
+static EEPROM_24LCxx eeprom(EEPROM_24LCxx_Type::LC64,
+                            EEPROM_I2C_DEV_ADDR,
+                            [](size_t const dev_addr) { Wire.beginTransmission(dev_addr); },
+                            [](uint8_t const data) { Wire.write(data); },
+                            []() { return Wire.endTransmission(); },
+                            [](uint8_t const dev_addr, size_t const len) -> size_t { return Wire.requestFrom(dev_addr, len); },
+                            []() { return Wire.available(); },
+                            []() { return Wire.read(); });
+
+static littlefs::FilesystemConfig filesystem_config
+  (
+    +[](const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) -> int
+    {
+      eeprom.read_page((block * c->block_size) + off, (uint8_t *)buffer, size);
+      return LFS_ERR_OK;
+    },
+    +[](const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) -> int
+    {
+      eeprom.write_page((block * c->block_size) + off, (uint8_t const *)buffer, size);
+      return LFS_ERR_OK;
+    },
+    +[](const struct lfs_config *c, lfs_block_t block) -> int
+    {
+      for(size_t off = 0; off < c->block_size; off += eeprom.page_size())
+        eeprom.fill_page((block * c->block_size) + off, 0xFF);
+      return LFS_ERR_OK;
+    },
+    +[](const struct lfs_config *c) -> int
+    {
+      return LFS_ERR_OK;
+    },
+    eeprom.page_size(),
+    eeprom.page_size(),
+    (eeprom.page_size() * 4), /* littlefs demands (erase) block size to exceed read/prog size. */
+    eeprom.device_size() / (eeprom.page_size() * 4),
+    500,
+    eeprom.page_size(),
+    eeprom.page_size()
+  );
+static littlefs::Filesystem filesystem(filesystem_config);
+
+#if __GNUC__ >= 11
+cyphal::support::platform::storage::littlefs::KeyValueStorage kv_storage(filesystem);
+#endif /* __GNUC__ >= 11 */
 
 /* REGISTER ***************************************************************************/
 
-static CanardNodeID node_id = DEFAULT_RADIATION_SENSOR_NODE_ID;
+static uint16_t     node_id           = std::numeric_limits<uint16_t>::max();
+static CanardPortID port_id_radiation = std::numeric_limits<CanardPortID>::max();
 
 #if __GNUC__ >= 11
 
 const auto node_registry = node_hdl.create_registry();
 
-const auto reg_rw_uavcan_node_id                = node_registry->expose("cyphal.node.id", {}, node_id);
+const auto reg_rw_uavcan_node_id                = node_registry->expose("cyphal.node.id", {true}, node_id);
 const auto reg_ro_uavcan_node_description       = node_registry->route ("cyphal.node.description", {true}, []() { return "L3X-Z Radiation Sensor"; });
-const auto reg_ro_uavcan_pub_radiation_cpm_id   = node_registry->route ("cyphal.pub.radiation_cpm.id", {true}, []() { return ID_RADIATION_VALUE; });
-const auto reg_ro_uavcan_pub_radiation_cpm_type = node_registry->route ("cyphal.pub.radiation_cpm.type", {true}, []() { return "uavcan.primitive.scalar.Integer16.1.0"; } );
+const auto reg_rw_uavcan_pub_radiation_cpm_id   = node_registry->expose("cyphal.pub.radiation_cpm.id", {true}, port_id_radiation);
+const auto reg_ro_uavcan_pub_radiation_cpm_type = node_registry->route ("cyphal.pub.radiation_cpm.type", {true}, []() { return "uavcan.primitive.scalar.Natural16.1.0"; } );
 
 #endif /* __GNUC__ >= 11 */
 
-static volatile int16_t rad_tick_cnt = 0;
+static volatile uint16_t rad_tick_cnt = 0;
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -104,16 +150,60 @@ static volatile int16_t rad_tick_cnt = 0;
 void setup()
 {
   Serial.begin(115200);
-  //while(!Serial) { Watchdog.reset(); } /* only for debug */
+  while(!Serial) { } /* only for debug */
+
+  Debug.prettyPrintOn(); /* Enable pretty printing on a shell. */
+
+  /* LITTLEFS/EEPROM ********************************************************************/
+  Wire.begin();
+
+  if (!eeprom.isConnected()) {
+    DBG_ERROR("Connecting to EEPROM failed.");
+    return;
+  }
+  Serial.println(eeprom);
+
+  if (auto const err_mount = filesystem.mount(); err_mount.has_value()) {
+    DBG_ERROR("Mounting failed with error code %d", static_cast<int>(err_mount.value()));
+    (void)filesystem.format();
+  }
+
+  if (auto const err_mount = filesystem.mount(); err_mount.has_value()) {
+    DBG_ERROR("Mounting failed again with error code %d", static_cast<int>(err_mount.value()));
+    return;
+  }
+
+#if __GNUC__ >= 11
+  auto const rc_load = cyphal::support::load(kv_storage, *node_registry);
+  if (rc_load.has_value()) {
+    DBG_ERROR("cyphal::support::load failed with %d", static_cast<int>(rc_load.value()));
+    return;
+  }
+#endif /* __GNUC__ >= 11 */
+
+  (void)filesystem.unmount();
+
+  /* If the node ID contained in the register points to an undefined
+   * node ID, assign node ID 0 to this node.
+   */
+  if (node_id > CANARD_NODE_ID_MAX)
+    node_id = 0;
+  node_hdl.setNodeId(static_cast<CanardNodeID>(node_id));
+
+  if (port_id_radiation != std::numeric_limits<CanardPortID>::max())
+    radiation_pub = node_hdl.create_publisher<uavcan::primitive::scalar::Natural16_1_0>(port_id_radiation, 1*1000*1000UL /* = 1 sec in usecs. */);
+
+  DBG_INFO("Node ID: %d\n\r\tRADIATION ID = %d",
+           node_id, port_id_radiation);
 
   /* NODE INFO **************************************************************************/
   static const auto node_info = node_hdl.create_node_info
   (
-    /* uavcan.node.Version.1.0 protocol_version */
+    /* cyphal.node.Version.1.0 protocol_version */
     1, 0,
-    /* uavcan.node.Version.1.0 hardware_version */
+    /* cyphal.node.Version.1.0 hardware_version */
     1, 0,
-    /* uavcan.node.Version.1.0 software_version */
+    /* cyphal.node.Version.1.0 software_version */
     0, 1,
     /* saturated uint64 software_vcs_revision_id */
 #ifdef CYPHAL_NODE_INFO_GIT_VERSION
@@ -122,42 +212,78 @@ void setup()
     0,
 #endif
     /* saturated uint8[16] unique_id */
-    OpenCyphalUniqueId(),
+    cyphal::support::UniqueId::instance().value(),
     /* saturated uint8[<=50] name */
     "107-systems.l3xz-radiation-sensor"
   );
 
   /* Setup LED pins and initialize */
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED2_PIN, LOW);
+  digitalWrite(LED3_PIN, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  /* Setup radition sensor pin and initialize. */
   pinMode(RADIATION_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(RADIATION_PIN), []() { rad_tick_cnt++; }, RISING);
 
   /* Setup SPI access */
   SPI.begin();
-  pinMode(MKRCAN_MCP2515_CS_PIN, OUTPUT);
-  digitalWrite(MKRCAN_MCP2515_CS_PIN, HIGH);
-
-  /* Attach interrupt handler to register MCP2515 signaled by taking INT low */
-  pinMode(MKRCAN_MCP2515_INT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(MKRCAN_MCP2515_INT_PIN), []() { mcp2515.onExternalEventHandler(); }, LOW);
+  SPI.beginTransaction(MCP2515x_SPI_SETTING);
+  pinMode(MCP2515_INT_PIN, INPUT_PULLUP);
+  pinMode(MCP2515_CS_PIN, OUTPUT);
+  digitalWrite(MCP2515_CS_PIN, HIGH);
 
   /* Initialize MCP2515 */
   mcp2515.begin();
   mcp2515.setBitRate(CanBitRate::BR_250kBPS_16MHZ);
+
+  CanardFilter const CAN_FILTER_SERVICES = canardMakeFilterForServices(node_id);
+  DBG_INFO("CAN Filter #1\n\r\tExt. Mask : %8X\n\r\tExt. ID   : %8X",
+           CAN_FILTER_SERVICES.extended_mask,
+           CAN_FILTER_SERVICES.extended_can_id);
+
+  /* Only pass service requests/responses for this node ID through to receive buffer #0. */
+  uint32_t const RXMB0_MASK = CAN_FILTER_SERVICES.extended_mask;
+  size_t const RXMB0_FILTER_SIZE = 2;
+  uint32_t const RXMB0_FILTER[RXMB0_FILTER_SIZE] =
+    {
+      MCP2515::CAN_EFF_BITMASK | CAN_FILTER_SERVICES.extended_can_id,
+      MCP2515::CAN_EFF_BITMASK | 0
+    };
+  mcp2515.enableFilter(MCP2515::RxB::RxB0, RXMB0_MASK, RXMB0_FILTER, RXMB0_FILTER_SIZE);
+
+  /* Only pass messages with ID 0 to receive buffer #1 (filtering out most). */
+  uint32_t const RXMB1_MASK = 0x01FFFFFF;
+  size_t const RXMB1_FILTER_SIZE = 4;
+  uint32_t const RXMB1_FILTER[RXMB1_FILTER_SIZE] =
+  {
+    MCP2515::CAN_EFF_BITMASK | 0,
+    MCP2515::CAN_EFF_BITMASK | 0,
+    MCP2515::CAN_EFF_BITMASK | 0,
+    MCP2515::CAN_EFF_BITMASK | 0
+  };
+  mcp2515.enableFilter(MCP2515::RxB::RxB1, RXMB1_MASK, RXMB1_FILTER, RXMB1_FILTER_SIZE);
+
+  /* Leave configuration and enable MCP2515. */
   mcp2515.setNormalMode();
 
-  /* set up radiation measurement */
-  attachInterrupt(digitalPinToInterrupt(RADIATION_PIN),
-                  []() { rad_tick_cnt++; },
-                  RISING);
+  DBG_INFO("Init complete.");
 }
 
 void loop()
 {
-  /* Process all pending OpenCyphal actions.
+  /* Deal with all pending events of the MCP2515 -
+   * signaled by the INT pin being driven LOW.
    */
-  {
-    CriticalSection crit_sec;
-    node_hdl.spinSome();
-  }
+  while(digitalRead(MCP2515_INT_PIN) == LOW)
+    mcp2515.onExternalEventHandler();
+
+  /* Process all pending Cyphal actions.
+   */
+  node_hdl.spinSome();
 
   /* Publish all the gathered data, although at various
    * different intervals.
@@ -167,7 +293,7 @@ void loop()
 
   unsigned long const now = millis();
 
-  if((now - prev_heartbeat) > 1000)
+  if((now - prev_heartbeat) > UPDATE_PERIOD_HEARTBEAT_ms)
   {
     prev_heartbeat = now;
 
@@ -179,26 +305,24 @@ void loop()
     msg.vendor_specific_status_code = 0;
 
     heartbeat_pub->publish(msg);
-   }
 
-  if((now - prev_radiation) > 10000)
+    digitalWrite(LED2_PIN, !digitalRead(LED2_PIN));
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+
+  if((now - prev_radiation) > UPDATE_PERIOD_RADIATION_ms)
   {
     prev_radiation = now;
 
-    Integer16_1_0 msg;
+    uavcan::primitive::scalar::Natural16_1_0 msg;
 
-    {
-      CriticalSection crit_sec;
-      msg.value = rad_tick_cnt;
-      rad_tick_cnt = 0;
-    }
+    msg.value = rad_tick_cnt;
+    rad_tick_cnt = 0;
 
-    radiation_tick_pub->publish(msg);
+    if (radiation_pub)
+      radiation_pub->publish(msg);
 
-    if (Serial) {
-      Serial.print("Radiation Value: ");
-      Serial.println(msg.value);
-    }
+    DBG_INFO("Radiation Value: %d", msg.value);
   }
 }
 
@@ -208,5 +332,48 @@ void loop()
 
 void onReceiveBufferFull(CanardFrame const & frame)
 {
+  digitalWrite(LED3_PIN, !digitalRead(LED3_PIN));
   node_hdl.onCanFrameReceived(frame);
+}
+
+ExecuteCommand::Response_1_1 onExecuteCommand_1_1_Request_Received(ExecuteCommand::Request_1_1 const & req)
+{
+  ExecuteCommand::Response_1_1 rsp;
+
+  if (req.command == ExecuteCommand::Request_1_1::COMMAND_RESTART)
+  {
+    if (auto const opt_err = cyphal::support::platform::reset_async(std::chrono::milliseconds(1000)); opt_err.has_value())
+    {
+      DBG_ERROR("reset_async failed with error code %d", static_cast<int>(opt_err.value()));
+      rsp.status = ExecuteCommand::Response_1_1::STATUS_FAILURE;
+      return rsp;
+    }
+    rsp.status = ExecuteCommand::Response_1_1::STATUS_SUCCESS;
+  }
+  else if (req.command == ExecuteCommand::Request_1_1::COMMAND_STORE_PERSISTENT_STATES)
+  {
+    if (auto const err_mount = filesystem.mount(); err_mount.has_value())
+    {
+      DBG_ERROR("Mounting failed with error code %d", static_cast<int>(err_mount.value()));
+      rsp.status = ExecuteCommand::Response_1_1::STATUS_FAILURE;
+      return rsp;
+    }
+#if __GNUC__ >= 11
+    auto const rc_save = cyphal::support::save(kv_storage, *node_registry);
+    if (rc_save.has_value())
+    {
+      DBG_ERROR("cyphal::support::save failed with %d", static_cast<int>(rc_save.value()));
+      rsp.status = ExecuteCommand::Response_1_1::STATUS_FAILURE;
+      return rsp;
+    }
+     rsp.status = ExecuteCommand::Response_1_1::STATUS_SUCCESS;
+#endif /* __GNUC__ >= 11 */
+    (void)filesystem.unmount();
+    rsp.status = ExecuteCommand::Response_1_1::STATUS_SUCCESS;
+  }
+  else {
+    rsp.status = ExecuteCommand::Response_1_1::STATUS_BAD_COMMAND;
+  }
+
+  return rsp;
 }
