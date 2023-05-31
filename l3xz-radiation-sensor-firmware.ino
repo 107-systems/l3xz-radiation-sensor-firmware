0b589427cfea6af759aae321ced427bb0946af9e
Alexander Entinger <consulting@lxrobotics.com>
2023-05-31 20:31:44 +0200

    Fix: increasing I2C clock to 400k reduces chance of watchdog timeout during storage.

--- l3xz-radiation-sensor-firmware.ino
+++ l3xz-radiation-sensor-firmware.ino
@@ -161,6 +161,7 @@ void setup()
 
   /* LITTLEFS/EEPROM ********************************************************************/
   Wire.begin();
+  Wire.setClock(400*1000UL); /* Set fast mode. */
 
   if (!eeprom.isConnected()) {
     DBG_ERROR("Connecting to EEPROM failed.");
